d9d98209946868219c00a96a3404cdd986f323e7
Shining-L <z18692872087@163.com>
2024-07-04 21:47:58 +0800

    修改数据库连接文件

--- models/base.py
+++ models/base.py
@@ -3,4 +3,5 @@ from flask_sqlalchemy import SQLAlchemy
 # The current Flask app is not registered with this 'SQLAlchemy' instance.
 # Did you forget to call 'init_app',
 # or did you create multiple 'SQLAlchemy' instances?
+
 db = SQLAlchemy()
